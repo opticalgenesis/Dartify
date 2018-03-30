2d3e45f4e21ab99d38e70cb35c03404b2edb013b
josh <jfeltimo@gmail.com>
2018-03-30 18:49:05 +0100

    I'm almost certain none of the playback will work, but something's there

--- lib/src/dartify_playback.dart
+++ lib/src/dartify_playback.dart
@@ -1,56 +1,78 @@
 import 'dart:html';
+import 'package:js/js.dart';
+@JS("https://sdk.scdn.co/spotify-player.js")
+library spotify;
 
 import 'dartify_utils.dart';
 
+@JS()
+class SpotifyPlayer {
+  external String name;
+  external getOauthToken getOauthToken(retrieveOauthToken);
+}
+
+@JS()
+external getOauthToken(SpotifyCallback cb);
+
+@JS()
+external class SpotifyCallback {
+  external void get callback;
+  external factory SpotifyCallback({void cb});
+}
+
+void retriveOauthToken(String accessToken) {
+  print(accessToken);
+}
+
 class DartifyPlayback {
 
-  static void PlayerPlay(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_PLAY}", "PUT",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerPlay(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_PLAY}", "PUT",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static void PlayerPause(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_PAUSE}", "PUT",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerPause(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_PAUSE}", "PUT",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static void PlayerNext(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_NEXT}", "POST",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerNext(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_NEXT}", "POST",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static void PlayerPrevious(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_PREVIOUS}", "POST",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerPrevious(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_PREVIOUS}", "POST",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static void PlayerShuffle(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_SHUFFLE}", "PUT",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerShuffle(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_SHUFFLE}", "PUT",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static void PlayerRepeat(String accessToken) {
-    HttpRequest req = _generateRequest(
-        "https://api.spotify.com${DartifyUtils.PLAYER_REPEAT}", "PUT",
-        accessToken);
-    req.send();
-  }
+  // static void PlayerRepeat(String accessToken) {
+  //   HttpRequest req = _generateRequest(
+  //       "https://api.spotify.com${DartifyUtils.PLAYER_REPEAT}", "PUT",
+  //       accessToken);
+  //   req.send();
+  // }
 
-  static HttpRequest _generateRequest(String url, String method,
-      String accessToken) {
-    HttpRequest req = new HttpRequest();
-    req.open(method, url);
-    req.setRequestHeader("Authorization", "Bearer $accessToken");
-    return req;
-  }
+  // static HttpRequest _generateRequest(String url, String method,
+  //     String accessToken) {
+  //   HttpRequest req = new HttpRequest();
+  //   req.open(method, url);
+  //   req.setRequestHeader("Authorization", "Bearer $accessToken");
+  //   return req;
+  // }
 }
